a91dc0049712085b9ba865c80310d6aafc8abb68
unknown <mpatajac@mathos.hr>
2020-11-04 22:18:07 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const url = "http://5e6a1a5baefe.ngrok.io"
+const api =  `${url}/api`;
+
+function send() {
+	const name = document.getElementById("name").value;
+	const title = document.getElementById("title").value;
+	const message = document.getElementById("message").value;
+	const code = document.getElementById("code-container").value;
+
+	const data = {
+		name,
+		title,
+		message,
+		code
+	};
+
+	$.ajax({
+		method: "POST",
+		url: `${api}/send`,
+		data: JSON.stringify(data),
+		contentType: 'application/json',
+	}).done((response) => {
+			if (response == "ok") {
+				console.log("Message sent!");
+				setStatusSpan("Message sent!", "green");
+			} else {
+				console.error("Something went wrong...");
+				setStatusSpan("Something went wrong...", "red");
+			}
+		}
+	);
+}
+
+function setStatusSpan(text, color) {
+	const statusSpan = document.getElementById("statusSpan");
+	statusSpan.innerHTML = text;
+	statusSpan.style.color = color;
+	statusSpan.style.display = "visible";
+	setTimeout(() => statusSpan.style.display = "none", 5000);
+}
